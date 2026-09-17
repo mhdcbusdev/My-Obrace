01ee164cbab80e87e10845c7a6363cd49fc088ba
mhdcbusdev <mhdc.busdev@gmail.com>
2026-09-17 11:44:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,6 +15,7 @@ window.MYOBRACE_CONFIG = {
      dengan tulisan PERSIS sama. Kalau nama beda satu huruf, notifikasi WA tidak terkirim
      (akan jatuh ke nomor cadangan/CRO). */
   CLINICS: [
+     "Medikids Test",
     "MHDC Bulog",
     "MHDC Kalibata City",
     "MHDC Menara Sentraya",
